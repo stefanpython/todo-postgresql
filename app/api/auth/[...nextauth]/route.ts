7fa3bbbf57fa7040147bc78ccea96148f6335056
stefanpython <stefanhtml@yahoo.com>
2025-04-01 14:19:26 +0300

    Create nextauth api route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,74 @@
+import NextAuth, { AuthOptions, SessionStrategy } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+import { compare } from "bcrypt";
+import { User } from "@prisma/client";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !(user as User & { password?: string }).password) {
+          return null;
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          (user as User & { password: string }).password
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          image: user.image,
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt" as SessionStrategy,
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async session({ session, token }: { session: any; token: any }) {
+      if (token && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const prisma = new PrismaClient();
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
