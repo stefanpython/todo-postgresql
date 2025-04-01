d4d05d06361ce5e271ff4533a5a07e6fe1a499ac
stefanpython <stefanhtml@yahoo.com>
2025-04-01 14:39:44 +0300

    Update root layout with authprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { TodoProvider } from "@/contexts/TodoContext";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TodoProvider>{children}</TodoProvider>
+        <AuthProvider>
+          <TodoProvider>{children}</TodoProvider>
+        </AuthProvider>
       </body>
     </html>
   );
